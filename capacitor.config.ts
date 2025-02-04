29de12dc16827cf4576e64412cd1fcef667ff3c6
Juan Pablo <gonzalez0421200@gmail.com>
2025-02-04 13:02:37 -0600

    Mi Primer Commit de mi Aplicacion

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'CapiLogros',
+  webDir: 'www'
+};
+
+export default config;
